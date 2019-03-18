fcd7751f614587af7ae10050fea71c209b5bf5cf
unknown <lappjeff20@gmail.com>
2019-03-18 15:44:59 -0400

    Imported header items into App.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import Content from './HeaderContent';
+import Image from './ImageThumbnail';
+
+
+const HeaderContainer = function() {
+    return (
+        <div className='header-container'>
+            <Image />
+            <Content />
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = function() {
+    return (
+        <section>
+            <div className='top-row'>
+            <h2>Lambda School</h2>
+            <p>@LambdaSchool - 26 Jan</p>
+            </div>
+        </section>
+    );
+}
+export default HeaderContent;
