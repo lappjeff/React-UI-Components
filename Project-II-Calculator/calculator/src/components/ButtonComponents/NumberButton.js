436e4ba3cdcdcf7f1a01549851b8330bc91ca300
unknown <lappjeff20@gmail.com>
2019-03-19 17:37:07 -0400

    Completed MVP

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Button.css';
 
-const numbers = [1,2,3,4,5,6,7,8,9]
 
 const NumberButton = function(props) {
   return (
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -4,30 +4,34 @@ import NumberButton from '../ButtonComponents/NumberButton'
 import ActionButton from '../ButtonComponents/ActionButton'
 
 const numbers = [1,2,3,4,5,6,7,8,9]
+const symbols = ['/', 'x', '-', '+', '=']
 
 const CalculatorDisplay = function() {
   return (
     <div className='container'>
+
       <div className='display'>
         <h1>0</h1>
       </div>
+
       <div className='main'>
-        <div className='side-row'>
-          <NumberButton buttonStyles='red-button' text='/'/>
-          <NumberButton buttonStyles='red-button' text='x'/>
-          <NumberButton buttonStyles='red-button' text='-'/>
-          <NumberButton buttonStyles='red-button' text='+'/>
-          <NumberButton buttonStyles='red-button' text='='/>
-        </div>
-        <div className='number-icons'>
+        <div className='main-rows'>
+          <ActionButton text='clear' />
+          <div className='number-icons'>
             {numbers.map(function(number) {
-              return <NumberButton text={number} />
+              return <NumberButton text={number} buttonStyles='fit-width'/>
             })}
+          </div>
+          <ActionButton text='0' />
+        </div>
+        <div className='side-row'>
+          {symbols.map(function(symbol) {
+            return <NumberButton buttonStyles='red-button' text={symbol} />
+          })}
         </div>
       </div>
     </div>
   )
 }
-
 
 export default CalculatorDisplay
