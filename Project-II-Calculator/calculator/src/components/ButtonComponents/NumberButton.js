7f2d85359678d6d37282e5cfc2277c8ba5bc9b25
unknown <lappjeff20@gmail.com>
2019-03-19 15:54:35 -0400

    Finished NumberButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = function(props) {
+  return (
+    <div className={`${props.buttonStyles}`}>{`${props.text}`}</div>
+  )
+}
+
+NumberButton.defaultProps = {
+  buttonStyles: 'defaultStyles'
+}
+
+export default NumberButton
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from '../ButtonComponents/NumberButton'
+
+
+const CalculatorDisplay = function() {
+  return (
+    <div className='container'>
+      <div className='display'>
+        <h1>0</h1>
+      </div>
+      <NumberButton buttonStyle='red' text='1'/>
+    </div>
+  )
+}
+
+export default CalculatorDisplay
